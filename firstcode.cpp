bf74623ab59e3f0b651db7315d0fc2bcc691da2e
Rakeshraj Mahakud <rakeshrajmahakud@Rakeshrajs-MBP.lan>
2024-09-03 21:27:45 +0530

    m first step towards dsa series

---
+++ firstcode.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @brief A simple program to print "hello world" to the console.
+ * 
+ * This program demonstrates a basic C++ program structure and
+ * the use of the standard output stream to print text.
+ */
+
+#include <iostream> // Include the iostream library for input and output operations
+
+using namespace std; // Use the standard namespace
+
+/**
+ * @brief The main function of the program.
+ * 
+ * This is the entry point of the program. It prints "hello world" to the console.
+ * 
+ * @return int Returns 0 to indicate successful execution.
+ */
+int main()
+{
+    cout << "hello world"; // Print "hello worold" to the console
+    return 0; // Return 0 to indicate successful execution
+}
